0283b4508ab4eef2dfb78040ffa683a584e4b93f
胡汇鑫 <18611551449@163.com>
2018-11-28 22:49:26 +0800

    添加消息体附加属性测试

---
+++ rabbit-api/src/main/java/com/hhx/rabbitmq/message/Consumer.java
@@ -0,0 +1,28 @@
+package com.hhx.rabbitmq.message;
+
+import com.hhx.rabbitmq.Util;
+import com.rabbitmq.client.QueueingConsumer;
+
+/**
+ * 消费者
+ * @author hhx
+ */
+public class Consumer {
+
+    public static void main(String[] args) {
+        Util.consumer(channel -> {
+            try {
+                QueueingConsumer queueingConsumer = new QueueingConsumer(channel);
+                // 关联消费者和队列
+                channel.basicConsume("test001", true, queueingConsumer);
+                // 获取消息
+                QueueingConsumer.Delivery delivery;
+                delivery = queueingConsumer.nextDelivery();
+                System.out.println("message: " + new String(delivery.getBody()));
+                System.out.println("properties: " + delivery.getProperties());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
